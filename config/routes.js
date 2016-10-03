f91831628237ca607261d79b430f3904f4873c5c
DESKTOP-KST0Q8K\rr <raulvidal228@gmail.com>
2016-10-02 22:26:45 -0400

    implementing Passport.js and dummy controller

--- config/routes.js
+++ config/routes.js
@@ -32,9 +32,18 @@ module.exports.routes = {
   *                                                                          *
   ***************************************************************************/
 
-  /*'/': {
-    view: 'homepage'
-  }*/
+
+  'get /login': {
+       view: 'login'
+  },
+
+  'post /login': 'AuthController.login',
+
+  '/logout': 'AuthController.logout',
+
+  'get /signup': {
+    view: 'signup'
+  }
 
   /***************************************************************************
   *                                                                          *
