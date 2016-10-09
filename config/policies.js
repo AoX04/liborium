f3f14c90e9cc353e0548ef36fd260f79113ae3be
DESKTOP-KST0Q8K\rr <raulvidal228@gmail.com>
2016-10-08 21:11:27 -0400

    adding libraryController to policies

--- config/policies.js
+++ config/policies.js
@@ -33,6 +33,9 @@ module.exports.policies = {
   },
     'BookController': {
     '*': 'isAuthenticated'
+  },
+    'LibraryController': {
+    '*': 'isAuthenticated'
   }
 
   /***************************************************************************
